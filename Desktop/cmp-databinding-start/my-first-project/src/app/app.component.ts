2497ae03d554a2e6e7e7be5316d0853c6d42fd9f
Nikita <nikis20879@gmail.com>
2021-04-30 20:08:44 -0500

    added modules

--- Desktop/cmp-databinding-start/my-first-project/src/app/app.component.ts
+++ Desktop/cmp-databinding-start/my-first-project/src/app/app.component.ts
@@ -6,23 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [];
-  newServerName = '';
-  newServerContent = '';
+  loadedFeature = 'recipe';
 
-  onAddServer() {
-    this.serverElements.push({
-      type: 'server',
-      name: this.newServerName,
-      content: this.newServerContent
-    });
-  }
-
-  onAddBlueprint() {
-    this.serverElements.push({
-      type: 'blueprint',
-      name: this.newServerName,
-      content: this.newServerContent
-    });
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
   }
 }
